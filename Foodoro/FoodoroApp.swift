cc21eceddcb9b7a6766ac1add6cbbda78147cf19
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-16 21:26:09 +0200

    Initial Commit

---
+++ Foodoro/FoodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodoroApp.swift
+//  Foodoro
+//
+//  Created by Hugo on 16.11.22.
+//
+
+import SwiftUI
+
+@main
+struct FoodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
