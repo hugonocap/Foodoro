929c92561dea27010e8dc0bd678cff282ba1fb95
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-19 16:28:09 +0200

    Header

--- Foodoro/Views/All Categories/AllCategoriesHeaderView.swift
+++ Foodoro/Views/All Categories/AllCategoriesHeaderView.swift
@@ -9,15 +9,38 @@ import SwiftUI
 
 struct AllCategoriesHeaderView: View {
     var body: some View {
-        HStack {
-            
+            HStack(spacing: 0) {
+                    
+                Button {
+                    //
+                } label: {
+                    GeneralButton(icon: "chevron.left")
+                }
+
+                    
+                    Spacer()
+                    
+                    Text("All Categories")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .fontDesign(.rounded)
+                        .padding(.trailing, 30)
+                       
+                        
+                    Spacer()
+            }
         }
-    }
 }
 
 struct AllCategoriesHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        AllCategoriesHeaderView()
-            .preferredColorScheme(.dark)
+        VStack {
+            AllCategoriesHeaderView()
+                .preferredColorScheme(.dark)
+                .padding(.horizontal)
+            
+            Spacer()
+        }
+           
     }
 }
