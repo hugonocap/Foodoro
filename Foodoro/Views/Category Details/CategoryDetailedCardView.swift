a1061a44f504960646ce82d0444f868cf1f4ef55
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-20 13:14:44 +0200

    detailed card view

--- Foodoro/Views/Category Details/CategoryDetailedCardView.swift
+++ Foodoro/Views/Category Details/CategoryDetailedCardView.swift
@@ -9,37 +9,77 @@ import SwiftUI
 
 struct CategoryDetailedCardView: View {
     var body: some View {
-        ZStack {
-            ZStack(alignment: .top) {
-                Color("AppGray")
-                    .frame(width: .infinity, height: 240)
-                    .cornerRadius(20)
-
-                Image("Mcdonalds")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: .infinity, height: 120)
-                    .clipped()
+        ZStack(alignment: .center) {
+            ZStack (alignment: .bottom) {
+                ZStack(alignment: .top) {
+                    Color("AppGray")
+                        .frame(width: .infinity, height: 240)
+                        .cornerRadius(20)
                     
-                HStack {
-                    Text("30 mins away")
-                        .font(.caption)
-                        .foregroundColor(.white)
-                        .fontWeight(.semibold)
-                        .padding(7)
-                        .background(.thickMaterial.opacity(0.6))
-                        .clipShape(Capsule())
+                    Image("Mcdonalds")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: .infinity, height: 120)
+                        .clipped()
                     
-                    Spacer()
-                    
-                    Image(systemName: "heart")
+                    HStack {
+                        Text("30 mins away")
+                            .font(.caption)
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                            .padding(7)
+                            .background(.thickMaterial.opacity(0.6))
+                            .clipShape(Capsule())
+                        
+                        Spacer()
+                        
+                        Image(systemName: "heart")
+                            .font(.title)
+                            .foregroundColor(.white)
+                    }
+                    .padding()
                 }
-                .padding()
+                
+                ZStack(alignment: .bottom) {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            // name
+                            Text("McDonalds")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            // rating
+                            HStack(spacing: 5) {
+                                HStack(spacing: 0) {
+                                    Image(systemName: "star.fill")
+                                    Text("4.6")
+                                }
+                                .foregroundColor(.yellow)
+                                
+                                Text("(244)")
+                                    .opacity(0.6)
+                            }
+                            .fontWeight(.semibold)
+                        }
+                        Spacer()
+                        VStack(alignment: .trailing) {
+                            // fee price
+                            Text("$1.00")
+                                .fontWeight(.bold)
+                            // delivery fee text
+                            Text("Delivery Fee")
+                                .fontWeight(.medium)
+                        }
+                    }
+                    .padding(.horizontal)
+                    .foregroundColor(.white)
+                }
+                .padding(.bottom)
             }
             
             ZStack(alignment: .center) {
                 Image("Mcdonalds")
                     .resizable()
+                    .scaledToFill()
                     .frame(width: 84, height: 84)
                     .cornerRadius(.infinity)
                     .overlay {
@@ -50,6 +90,7 @@ struct CategoryDetailedCardView: View {
             
         }
         .cornerRadius(20)
+        .fontDesign(.rounded)
     }
     
     struct CategoryDetailedCardView_Previews: PreviewProvider {
