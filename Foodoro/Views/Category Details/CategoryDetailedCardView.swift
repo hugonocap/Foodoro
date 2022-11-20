fa4ca7c9f8ddab522908207be3602741b5ae53b5
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-20 16:23:15 +0200

    state property for favourite button

--- Foodoro/Views/Category Details/CategoryDetailedCardView.swift
+++ Foodoro/Views/Category Details/CategoryDetailedCardView.swift
@@ -8,102 +8,117 @@
 import SwiftUI
 
 struct CategoryDetailedCardView: View {
+    @State private var isActive: Bool = false
     var body: some View {
-        ZStack(alignment: .center) {
-            ZStack (alignment: .bottom) {
-                ZStack(alignment: .top) {
-                    Color("AppGray")
-                        .frame(width: .infinity, height: 240)
-                        .cornerRadius(20)
+        Button {
+            //
+        } label: {
+            ZStack(alignment: .center) {
+                ZStack (alignment: .bottom) {
+                    ZStack(alignment: .top) {
+                        Button {
+                            
+                        } label: {
+                            Color("AppGray")
+                                .frame(width: .infinity, height: 240)
+                                .cornerRadius(20)
+                        }
+                        
+                        Image("Mcdonalds")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: .infinity, height: 120)
+                            .clipped()
+                        
+                        HStack {
+                            Text("30 mins away")
+                                .font(.caption)
+                                .foregroundColor(.white)
+                                .fontWeight(.semibold)
+                                .padding(7)
+                                .background(.thickMaterial.opacity(0.6))
+                                .clipShape(Capsule())
+                            
+                            Spacer()
+                            
+                            Button {
+                                isActive.toggle()
+                            } label: {
+                                if isActive {
+                                    Image(systemName: "heart.fill")
+                                        .font(.title)
+                                        .foregroundColor(Color("AppPink"))
+                                } else {
+                                    Image(systemName: "heart")
+                                        .font(.title)
+                                        .foregroundColor(.white)
+                                }
+                            }
+                            
+                        }
+                        .padding()
+                    }
                     
+                    ZStack(alignment: .bottom) {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                // name
+                                Text("McDonalds")
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                // rating
+                                HStack(spacing: 5) {
+                                    HStack(spacing: 0) {
+                                        Image(systemName: "star.fill")
+                                        Text("4.6")
+                                    }
+                                    .foregroundColor(.yellow)
+                                    
+                                    Text("(244)")
+                                        .opacity(0.6)
+                                }
+                                .fontWeight(.semibold)
+                            }
+                            Spacer()
+                            VStack(alignment: .trailing) {
+                                // fee price
+                                Text("$1.00")
+                                    .fontWeight(.bold)
+                                // delivery fee text
+                                Text("Delivery Fee")
+                                    .fontWeight(.medium)
+                            }
+                        }
+                        .padding(.horizontal)
+                        .foregroundColor(.white)
+                    }
+                    .padding(.bottom)
+                }
+                
+                ZStack(alignment: .center) {
                     Image("Mcdonalds")
                         .resizable()
                         .scaledToFill()
-                        .frame(width: .infinity, height: 120)
-                        .clipped()
-                    
-                    HStack {
-                        Text("30 mins away")
-                            .font(.caption)
-                            .foregroundColor(.white)
-                            .fontWeight(.semibold)
-                            .padding(7)
-                            .background(.thickMaterial.opacity(0.6))
-                            .clipShape(Capsule())
-                        
-                        Spacer()
-                        
-                        Image(systemName: "heart")
-                            .font(.title)
-                            .foregroundColor(.white)
-                    }
-                    .padding()
+                        .frame(width: 84, height: 84)
+                        .cornerRadius(.infinity)
+                        .overlay {
+                            Circle()
+                                .stroke(Color("AppGray"), lineWidth: 7)
+                        }
                 }
                 
-                ZStack(alignment: .bottom) {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            // name
-                            Text("McDonalds")
-                                .font(.title3)
-                                .fontWeight(.bold)
-                            // rating
-                            HStack(spacing: 5) {
-                                HStack(spacing: 0) {
-                                    Image(systemName: "star.fill")
-                                    Text("4.6")
-                                }
-                                .foregroundColor(.yellow)
-                                
-                                Text("(244)")
-                                    .opacity(0.6)
-                            }
-                            .fontWeight(.semibold)
-                        }
-                        Spacer()
-                        VStack(alignment: .trailing) {
-                            // fee price
-                            Text("$1.00")
-                                .fontWeight(.bold)
-                            // delivery fee text
-                            Text("Delivery Fee")
-                                .fontWeight(.medium)
-                        }
-                    }
-                    .padding(.horizontal)
-                    .foregroundColor(.white)
-                }
-                .padding(.bottom)
             }
-            
-            ZStack(alignment: .center) {
-                Image("Mcdonalds")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 84, height: 84)
-                    .cornerRadius(.infinity)
-                    .overlay {
-                        Circle()
-                            .stroke(Color("AppGray"), lineWidth: 7)
-                    }
-            }
-            
+            .cornerRadius(20)
+            .fontDesign(.rounded)
         }
-        .cornerRadius(20)
-        .fontDesign(.rounded)
     }
     
     struct CategoryDetailedCardView_Previews: PreviewProvider {
         static var previews: some View {
             CategoryDetailedCardView()
+                .preferredColorScheme(.dark)
                 .padding(.horizontal)
         }
     }
 }
 
-struct CircleShape: View {
-    var body: some View {
-        Circle()
-            .strokeBorder(.black)
-    }
-}
